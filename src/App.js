2847328aa7d1a6d5e74346a9ee53b9cc601b218f
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-06-07 20:50:17 -0300

    update app js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 // Components
-import NavBar from "./components/NavBar/NavBar.js";
+import NavBar from "./NavBar";
 
 class App extends Component {
 	render () {
