9e265199606d007db8eb3339de8daadd2e7c41ea
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-06-09 18:00:59 -0300

    commit header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+
+const Header = () => {
+        return (
+            <div className='Header'>
+                <h1>Soy un titulo</h1>
+                <h2>Soy un subtitulo</h2>
+            </div>
+        );
+};
+
+
+export default Header;
