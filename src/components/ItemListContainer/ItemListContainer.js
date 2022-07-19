c7417895c9490f70765fce9b37cfd819909f3b49
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-07-19 04:36:33 -0300

    Commit

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import './ItemListContainer.css';
-// import { data } from '../../Data/Data';
 
 
 const ItemListContainer = () => {
-
-    // const [items, setItems] = useState([]);
-
-    // useEffect(() => {
-
-    //     setTimeout(() => { setItems(data); }, 2000);
-
-    // }, []);
 
 
 
